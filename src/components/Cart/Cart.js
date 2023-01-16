d455a354f349f21064912dfe0a3226a93a0281e1
João Gonçalves <joaopedro.jp.4998@gmail.com>
2023-01-16 17:13:05 +0000

    Make items remoble and addable

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,8 +12,12 @@ const Cart = (props) => {
   let hasItems = false;
   cartCtx.items.length > 0 ? (hasItems = true) : (hasItems = false);
 
-  const cartItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
@@ -24,7 +28,7 @@ const Cart = (props) => {
           amount={item.amount}
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
